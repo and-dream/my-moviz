b175991c9947585e5149c843117d8bc7c8dd386c
Gloria <gloriadamase@gmail.com>
2023-09-11 16:41:40 +0200

    'font'

--- components/Home.jsx
+++ components/Home.jsx
@@ -4,8 +4,12 @@ import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div>
-        <header>
+    <div className={styles.main}>
+        <header className="styles.header">
+          <div className="{styles.logocontainer}">
+            <img src="logoletter.png" alt="" />
+            <img src="logo.png" alt="" />
+          </div>
             
         </header>
         <h1>last releases</h1>
