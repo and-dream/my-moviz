772a2b61a9477b9bd9569583441f61b5f92dcd48
Gloria <gloriadamase@gmail.com>
2023-09-11 16:24:42 +0200

    'moviz'

---
+++ components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from '../styles/Home.module.css';
+
+
+export default function Home() {
+  return (
+    <div>
+        <header>
+            
+        </header>
+        <h1>last releases</h1>
+    </div>
+  )
+}
